05ac13b9e4b4a065af180cf01633c7076cdba9ca
Changqing-zheng <1109300209@qq.com>
2018-12-08 11:55:59 +0800

    react

--- react-app/src/components/App.js
+++ react-app/src/components/App.js
@@ -1,13 +1,19 @@
-import React, { Component } from 'react';
+import React,{Component} from 'react';
+import { BrowserRouter as Router,Route } from 'react-router-dom';
+import Home from './Home';
+import Detail from './Detail';
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-            hhh]
-      </div>
-    );
-  }
+export default class App extends Component {
+    render(){
+        return(
+            <div>
+                <Router>
+                    <div>
+                        <Route path='/' component={Home}></Route>
+                        <Route path='/detail' component={Detail}></Route>
+                    </div>
+                </Router>
+            </div>
+        )
+    }
 }
-
-export default App;
